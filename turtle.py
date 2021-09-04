2cf6c3cb487ca47bd1bf69065a555378c8f174ab
Nivesh-GitHub <89345698+Nivesh-GitHub@users.noreply.github.com>
2021-09-04 09:11:49 +0530

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,12 +1,17 @@
-# Python program to draw
-# Rainbow Benzene
-# using Turtle Programming
-import turtle
-colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
-t = turtle.Pen()
-turtle.bgcolor('black')
-for x in range(360):
-	t.pencolor(colors[x%6])
-	t.width(x/100 + 1)
-	t.forward(x)
-	t.left(59)
+from tkinter import *
+root = Tk()
+root.title("Paint Application")
+root.geometry("500x350")
+def paint(event):
+    # get x1, y1, x2, y2 co-ordinates
+    x1, y1 = (event.x-3), (event.y-3)
+    x2, y2 = (event.x+3), (event.y+3)
+    color = "black"
+    # display the mouse movement inside canvas
+    wn.create_oval(x1, y1, x2, y2, fill=color, outline=color)
+# create canvas
+wn=Canvas(root, width=500, height=350, bg='white')
+# bind mouse event with canvas(wn)
+wn.bind('<B1-Motion>', paint)
+wn.pack()
+root.mainloop()
